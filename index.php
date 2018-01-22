cadd97a215bac01b990decacae8137627ad268fd
Arjay Antonio <arjay.antonio_9@yahoo.com>
2018-01-22 19:43:22 +0800

    12 Days of Christmas

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name = "viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
 	<title><?php echo getTitle();?> Lyrics</title>
 	<link href="https://fonts.googleapis.com/css?family=Mountains+of+Christmas" rel="stylesheet"> 
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
